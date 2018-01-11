98d92ac90fc4070c02bee6339f1afafa65f3875e
Huseyin F AKAR <hfakar@gmail.com>
2018-01-10 22:55:20 -0500

    refactoring and adding reactives

---
+++ forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signUpForm: FormGroup ;
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    });
+  }
+  onSubmit() {
+
+  }
+}
